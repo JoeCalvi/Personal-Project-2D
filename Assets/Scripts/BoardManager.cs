bdf0264c36ab4bd71e1c50982dd01e5ca3bf0972
JoeCalvi <joecalvi@me.com>
2023-06-25 12:14:36 -0600

    BoardManager done

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,11 +1,10 @@
 using System.Collections;
+// allows the use of lists
 using System.Collections.Generic;
 using UnityEngine;
 
 // allows use of "Serializable" attribute
 using System;
-// allows the use of lists
-using System.Collections.Generic;
 // tells Random to use the Unity Engine random number generator
 using Random = UnityEngine.Random;
 
@@ -90,15 +89,62 @@ public class BoardManager : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    //RandomPosition returns a random position from our list gridPositions.
+    Vector3 RandomPosition() 
     {
+        //Declare an integer randomIndex, set it's value to a random number between 0 and the count of items in our List gridPositions.
+        int randomIndex = Random.Range(0, gridPositions.Count);
         
+        //Declare a variable of type Vector3 called randomPosition, set it's value to the entry at randomIndex from our List gridPositions.
+        Vector3 randomPosition = gridPositions[randomIndex];
+
+        //Remove the entry at randomIndex from the list so that it can't be re-used.
+        gridPositions.RemoveAt(randomIndex);
+
+        //Return the randomly selected Vector3 position.
+        return randomPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    //LayoutObjectAtRandom accepts an array of game objects to choose from along with a minimum and maximum range for the number of objects to create.
+    void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
     {
-        
+        //Choose a random number of objects to instantiate within the minimum and maximum limits
+        int objectCount = Random.Range(minimum, maximum + 1);
+
+        //Instantiate objects until the randomly chosen limit objectCount is reached
+        for(int i = 0; i < objectCount; i++)
+        {
+            //Choose a position for randomPosition by getting a random position from our list of available Vector3s stored in gridPosition
+            Vector3 randomPosition = RandomPosition();
+
+            //Choose a random tile from tileArray and assign it to tileChoice
+            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+
+            //Instantiate tileChoice at the position returned by RandomPosition with no change in rotation
+            Instantiate (tileChoice, randomPosition, Quaternion.identity);
+        }
+    }
+
+    //SetupScene initializes our level and calls the previous functions to lay out the game board
+    public void SetupScene(int level) 
+    {
+        BoardSetup(); //Creates the outer walls and floor.
+
+        InitialiseList(); //Reset our list of gridpositions.
+
+        //Instantiate a random number of wall tiles based on minimum and maximum, at randomized positions.
+        LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.maximum);
+
+        //Instantiate a random number of food tiles based on minimum and maximum, at randomized positions.
+        LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.maximum);
+
+        //Determine number of enemies based on current level number, based on a logarithmic progression
+        int enemyCount = (int)Mathf.Log(level, 2f);
+
+        //Instantiate a random number of enemies based on minimum and maximum, at randomized positions.
+        LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount);
+
+        //Instantiate the exit tile in the upper right hand corner of our game board
+        Instantiate(exit, new Vector3(columns - 1, rows - 1, 0f), Quaternion.identity);
     }
 }
